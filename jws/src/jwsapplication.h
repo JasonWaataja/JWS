79468f6b2e25aec5a94a2091ce745a5aa2ce15ce
Jason Waataja <jasonswaataja@gmail.com>
2016-08-06 21:01:27 -0700

    Updated licensing for jws

--- jws/src/jwsapplication.h
+++ jws/src/jwsapplication.h
@@ -1,4 +1,19 @@
+/* jwsapplication.h - header for the JwsApplication class
 
+This file is part of JWS.
+
+JWS is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+JWS is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with JWS.  If not, see <http://www.gnu.org/licenses/>.  */
 
 #ifndef JWSAPPLICATION_H
 #define JWSAPPLICATION_H
--- jws/src/jwsinfo.h
+++ jws/src/jwsinfo.h
@@ -1,4 +1,19 @@
+/* jwsinfo.h - header for JwsInfo class
 
+This file is part of JWS.
+
+JWS is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+JWS is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with JWS.  If not, see <http://www.gnu.org/licenses/>.  */
 
 #ifndef JWSINFO_H
 #define JWSINFO_H
