08125ffa4f56432190d421d4f5d76ec49377e59b
Jason Waataja <jasonswaataja@gmail.com>
2016-08-10 18:17:13 -0700

    Added about dialog

--- jws-config/src/jwsconfigwindow.c
+++ jws-config/src/jwsconfigwindow.c
@@ -1561,6 +1561,35 @@ about_activated (GSimpleAction *action,
        GVariant *parameter,
        gpointer win)
 {
+  const gchar *authors[] = 
+    {
+      "Jason Waataja",
+      NULL
+    };
+
+  const gchar *license = 
+    _("This program is free software: you can redistribute it and/or modify\n"
+      "it under the terms of the GNU General Public License as published by\n"
+      "the Free Software Foundation, either version 3 of the License, or\n"
+      "(at your option) any later version.\n"
+      "\n"
+      "This program is distributed in the hope that it will be useful,\n"
+      "but WITHOUT ANY WARRANTY; without even the implied warranty of\n"
+      "MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the\n"
+      "GNU General Public License for more details.\n"
+      "\n"
+      "You should have received a copy of the GNU General Public License\n"
+      "along with this program.  " /* I broke here to stay under 80.  */
+      "If not, see <http://www.gnu.org/licenses/>.");
+
+  gtk_show_about_dialog (GTK_WINDOW (win),
+                         "program-name", "JWS-Config",
+                         "title", _("About JWS-Config"),
+                         "authors", authors,
+                         "copyright", "Copyright (C) 2016 Jason Waataja",
+                         "license", license,
+                         NULL);
+
 }
 
 gboolean
